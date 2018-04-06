cc2ebdf28f70cdf825b77d5e76574cba47e22ae6
Skander <skander.kc@gmail.com>
2018-04-06 00:09:55 -0400

    FIX save query

--- Server/src/controllers/queries.controller.js
+++ Server/src/controllers/queries.controller.js
@@ -5,10 +5,14 @@ class QueriesController {
   async new (req, res) {
     const user = await User.findById(req.user.id)
     const query = new Query(req.body)
-    // FIXME: It pushes ref of queries without creating query
+    // Save query
+    query.user = user.id
+    await query.save()
+    // Assign query to current user
     user.queries.push(query)
     user.save()
-    res.send(user.queries)
+
+    res.send({user, query})
   }
 }
 
